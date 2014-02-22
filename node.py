60ec4f3f7fdec807bf98d14b2717eadee81fc66c
Jackie Lee <jackylee0424@gmail.com>
2014-02-21 17:52:23 -0800

    build face recog pipeline

--- node.py
+++ node.py
@@ -12,18 +12,58 @@ import urllib2
 import time
 import sys
 import json
+import cv2
+from tinyfacerec.model import EigenfacesModel
+from tinyfacerec.model import FisherfacesModel
+from tinyfacerec.distance import CosineDistance
+import pickle
 
 tornado.options.define("port", default=8080, help="run on the given port", type=int)
+
+def imgpreprocess(filepath):
+    im = cv2.imread(filepath)
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    im = cv2.resize(im,(128, 128), interpolation = cv2.INTER_CUBIC)
+    im = cv2.equalizeHist(im)
+    return im
 
 class WSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         print "ws opened"
         self.dir_name = "%.0f"%(time.time()*1000.0)
         self.mode = 0
+        self.model_loaded = False
+        self.mmodel = None
+        self.labeldict = dict(ts=time.time())
+        self.load_data()
+        self.genesis_ts = 1392697800000
+    
+    
+    def load_data(self):
+        with open('data//model//model.bin', 'rb') as input:
+            self.mmodel = pickle.load(input)
+            self.model_loaded = True
+        with open('data//labels.bin', 'rb') as input:
+            self.labeldict = pickle.load(input)
+            print self.labeldict
+    
+    def detect_face(self, img):
+        t = time.time()
+        output_name, min_dist =  self.mmodel["eigen"].predict(img,-.5)
+        try:
+            output_label = self.labeldict[str(output_name+self.genesis_ts)]
+        except:
+            print "no mapped label found"
+            output_label = str(output_name+self.genesis_ts)
+        
+        print "predicted (eigen)= %s (%f). took %.3f ms"%(output_label, min_dist,(time.time()-t)*1000.)
+        t = time.time()
+        output_name, min_dist =  self.mmodel["fisher"].predict(img,-.5)
+        print "predicted (fisher)= %s (%f). took %.3f ms"%(output_label, min_dist,(time.time()-t)*1000.)
 
     def allow_draft76(self):
         # for iOS 5.0 Safari
-        return True
+        return False
     
     def on_message(self, message):
         parsed = tornado.escape.json_decode(message)
@@ -31,20 +71,29 @@ class WSocketHandler(tornado.websocket.WebSocketHandler):
 
         d = urllib2.unquote(parsed["base64Data"])
         img = base64.b64decode(d.split(',')[1])
+        fname = "%.0f"%(time.time()*1000.0)
+        
+        if not os.path.exists("data//raw//"+self.dir_name):
+            os.makedirs("data//raw//"+self.dir_name)
+        
+        fullpath = "data//raw//%s//%s.png"%(self.dir_name,fname)
+        with open(fullpath,"wb") as f:
+            f.write(img)
+            print "saved to %s.png"%fname
                                
         if (self.mode>0):
             print "Detect mode"
+            self.detect_face(imgpreprocess(fullpath))
+        
         else:
             print "Training mode"
-            print "current label: %s"%parsed["label"]
-            fname = "%.0f"%(time.time()*1000.0)
-            if not os.path.exists("data//raw//"+self.dir_name):
-                os.makedirs("data//raw//"+self.dir_name)
-            with open("data//raw//%s//%s.png"%(self.dir_name,fname),"wb") as f:
-                f.write(img)
-                print "saved to %s.png"%fname
-    
+            self.labeldict[self.dir_name]=parsed["label"]
+            print "Labels- %s"%(self.labeldict[self.dir_name])
+
     def on_close(self):
+        # save label pairs to disk
+        with open('data//labels.bin', 'wb') as output:
+            pickle.dump(self.labeldict, output, pickle.HIGHEST_PROTOCOL)
         print "ws closed"
 
 
